dc19b7da36fd3ef731974ff2457a312757936487
aajaber1 <>
2021-06-02 12:32:50 +0000

    this javascript file was created in replit

---
+++ java_script_file.js
@@ -0,0 +1,11 @@
+var question = prompt('Do you like cars')
+
+if(question == 'No' ||question == 'no' ){
+  alert('Get out :p')
+}
+else if (question == 'Yes' ||question == 'yes' ){
+  alert('you may enter :p')
+}
+else{
+  alert('Provide an answer or you won\'t enter this website ')
+}
