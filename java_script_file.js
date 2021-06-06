36e3f06b70bba185af310633d03040662b4870ce
aajaber1 <>
2021-06-06 11:11:11 +0000

    lab 07

--- java_script_file.js
+++ java_script_file.js
@@ -1,11 +1,46 @@
-var question = prompt('Do you like cars')
 
-if(question == 'No' ||question == 'no' ){
-  alert('Get out :p')
+var correctAnswer = 'yes';
+var correctAnswer2 = 'Yes'
+
+var wrongAnswer = 'no'
+var wrrongAnswer2 = 'No'
+
+var numberOfStars=5
+
+
+function wlecoming(){
+alert('Welcome to my fav. cars page')
 }
-else if (question == 'Yes' ||question == 'yes' ){
-  alert('you may enter :p')
+
+wlecoming();
+
+var question = prompt('Do you like cars')
+var attempt = 2;
+
+for (var i = 1; i < attempt; i++) {
+  if (question == correctAnswer2 || question == correctAnswer) {
+    alert('you may enter :p')
+    
+     break;
+  }
+else if (question == wrrongAnswer2 || question == wrongAnswer) {
+    var question = prompt('Do you like cars')
+    if(i<=2)
+  {
+    alert("get out :p")
+  }
+  }
 }
-else{
-  alert('Provide an answer or you won\'t enter this website ')
+
+var question2 = prompt('how many stars would you give this page (1-5)')
+
+function printStars(){
+while(question2 <=5)
+{
+  document.write('<div>' +' users rating for this page'+'</div>' +'<br>' )
+  for(var i = 0; i < question2; i++)
+  document.write('<div>'+ '<img src="goldenStar.jpg" width="50" height="50">'+ "</div>")
+  break;
 }
+}
+printStars();
